4acd3cff74c0a8daa1ff5a66ce8ab5832a3a838d
sublimnl <x@sublim.nl>
2023-05-07 00:35:28 -0400

    dsf

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,23 +1,19 @@
 import React from "react";
+/**
+ * @interface
+ * @property {string} name Name of the superhero
+ * @property {string} color Hex or color name for the text
+ * @property {string} background Hex or color name for the background
+ */
 interface IProps {
     name: string;
     color: string;
     background: string;
 }
-interface IState {
-}
 /**
- * CallSuperHeroButton
- *
- * Test description
- *
- * @param {{
- * name: "name of the super hero to call";
- * color: "hex or color name";
- * background: "hex or color name"
- * }}
+ * Creates a large Call Super Hero button.
  */
-export class CallSuperHeroButton extends React.Component<IProps, IState> {
+export class CallSuperHeroButton extends React.Component<IProps> {
     render(): React.JSX.Element;
 }
 
